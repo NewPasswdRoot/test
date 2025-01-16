2cc74327b46c261b936d375b3ea3c98ea3b8ecc1
Vladimir Danilov <Kllpups@hotmail.com>
2025-01-16 10:46:49 +0300

    Пользователь вводит целое число. Выведите его строку-описание вида "отрицательное четное число", "нулевое число", "положительное нечетное число", например, численным описанием числа 190 является строка "положительное четное число". Если число не является четным - выведите сообщение "число не является четным"

--- test/test.py
+++ test/test.py
@@ -1,1 +1,11 @@
-
+a = int(input())
+if a > 0:
+	print("Положительное ", end="")
+elif a < 0:
+	print("Отрицательное ", end="")
+if a == 0:
+	print("Нулевое число ")
+elif a%2 == 0:
+	print("четное число ")
+else:
+	print("нечетное число")
